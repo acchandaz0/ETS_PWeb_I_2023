f0678e57ed84644dec2102ab9aef0e4172f9754c
Arsya Dewi L <123639451+acchandaz0@users.noreply.github.com>
2023-10-18 09:23:30 +0700

    Update ETS.js

--- ETS.js
+++ ETS.js
@@ -1,36 +1,31 @@
-function getResult() {
-    const apiUrl = 'https://it-its.id/api/movies';
+<!DOCTYPE html>
+<html lang="en">
+ 
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ETS PWeb Arsya</title>
+    <link href="css/sb-admin-2.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-    fetch(apiUrl)
-        .then(response => response.text())
-        .then(data => {
-            console.log(data)
-            const res = JSON.parse(data);
+</head>
 
-            for(let i = 0; i < res.length; i++){
-                let obj = res[i];
-                const Title = obj.Title
-                const Plot = obj.Plot
-                let divi = `
-                <div class="col-xl-3 col-md-3 mb-4">
-                    <div class="card border-left-primary shadow h-100 py-2">
-                        <div class="card-body">
-                            <div class="row no-gutters align-items-center">
-                                <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                        ${Title}</div>
-                                    <div class="mb-0 font-weight-bold text-gray-800">${Plot}</div>
-                                </div>
-                            </div>
-                        </div>
+<body onload="getResult()">
+
+    <div id="wrapper">  
+        <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content">
+                <div class="container-fluid">
+                    <div class="d-md-flex align-items-center justify-content-between mb-4 mt-4">
+                        <div class="row" id="movieee"></div>
                     </div>
                 </div>
-                `;
-                document.getElementById('movieee').innerHTML = document.getElementById('movieee').innerHTML + divi;
-            }
-            
-        })
-        .catch(error => {
-            console.error(error);
-        })
-}
+            </div>
+        </div>
+    </div>
+
+    <script src="ETS.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</head>
