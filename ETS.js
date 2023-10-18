08447ec1c8f50015b5d98c6ce26f8055acc6cbd2
Arsya Dewi L <123639451+acchandaz0@users.noreply.github.com>
2023-10-18 09:55:23 +0700

    Update ETS.js

--- ETS.js
+++ ETS.js
@@ -12,13 +12,13 @@ function getResult() {
                 const Title = obj.Title
                 const Plot = obj.Plot
                 let divi = `
-                <div class="col-xl-3 col-md-3 mb-4">
-                    <div class="card shadow h-100 py-2">
+                <div class="col-xl-6 col-md-6 mb-4">
+                    <div class="card shadow h-100 py-1">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
-                                <div class="col mr-2">
-                                    <div class="font-weight-bold text-black-800 text-uppercase mb-1">${Title}</div>
-                                    <div class="text-xs mb-0 font-weight-bold text-black-800">${Plot}</div>
+                                <div class="col mr-2 p-3">
+                                    <div class="font-weight-bold text-black-800 mb-1">${Title}</div>
+                                    <div class="text-s mb-0 font-weight-regular text-black-800">${Plot}</div>
                                 </div>
                             </div>
                         </div>
