7c392900913ef4f35a97e3d716d6d8f0db7f7eb5
Arsya Dewi L <123639451+acchandaz0@users.noreply.github.com>
2023-10-18 09:07:36 +0700

    Add files via upload

---
+++ ETS.js
@@ -0,0 +1,36 @@
+function getResult() {
+    const apiUrl = 'https://it-its.id/api/movies';
+
+    fetch(apiUrl)
+        .then(response => response.text())
+        .then(data => {
+            console.log(data)
+            const res = JSON.parse(data);
+
+            for(let i = 0; i < res.length; i++){
+                let obj = res[i];
+                const Title = obj.Title
+                const Plot = obj.Plot
+                let divi = `
+                <div class="col-xl-3 col-md-3 mb-4">
+                    <div class="card border-left-primary shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                        ${Title}</div>
+                                    <div class="mb-0 font-weight-bold text-gray-800">${Plot}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                `;
+                document.getElementById('movieee').innerHTML = document.getElementById('movieee').innerHTML + divi;
+            }
+            
+        })
+        .catch(error => {
+            console.error(error);
+        })
+}
